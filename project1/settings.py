e6e8f03e256fe629e0bbe646d4c7b40417589837
tiz5001 <iizuka@wd6.so-net.ne.jp>
2023-08-31 14:06:27 +0900

    from SakuraVPS 8/31

--- project1/settings.py
+++ project1/settings.py
@@ -1,10 +1,18 @@
 from .settings_common import *
 
+from dotenv import load_dotenv
+
+import os
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(env_path)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY ='django-insecure-cl6$%!8&8c)qp#2(&1#f)(#a#o)rwogpm+dp5mx=ikuvwfiun!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS') ]
+ALLOWED_HOSTS = [os.environ.get('DOMAIN')]
+
+
 
